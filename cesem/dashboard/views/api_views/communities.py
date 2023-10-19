804e29a40a07315655952ad7cbad741331de2798
Javier Gutierrez <j_gutierrez-itind@kw.com>
2023-10-18 22:29:43 -0500

    feat: update subactivity sum

--- cesem/dashboard/views/api_views/communities.py
+++ cesem/dashboard/views/api_views/communities.py
@@ -27,8 +27,42 @@ class CommunityPathSerializer(BasePathSerializer):
             "zone": {"hidden": True},
             "zone_2": {"hidden": True},
         }
+        extra_kwargs = {
+            "zone": {"write_only": True},
+            "zone_2": {"write_only": True},
+        }
 
+class CommunityDetailsPathSerializer(BasePathSerializer):
+    zona_a = serializers.StringRelatedField(many=False, source="zone")
+    zona_b = serializers.StringRelatedField(many=False, source="zone_2")
+
+    @staticmethod
+    def get_path():
+        return "communities"
+
+    class Meta:
+        model = Community
+        fields = [
+            "name",
+            "url",
+            "zona_a",
+            "zona_b",
+            "zone",
+            "zone_2",
+            "url"
+        ]
+
+        custom_kwargs = {
+            "zone": {"hidden": True},
+            "zone_2": {"hidden": True},
+        }
+ 
 
 class CommunityViewSet(viewsets.ModelViewSet):
     queryset = Community.objects.select_related("zone", "zone_2").all()
     serializer_class = CommunityPathSerializer
+    serializer_details_class = CommunityDetailsPathSerializer
+    
+    def retrieve(self, request, *args, **kwargs):
+        self.serializer_class = self.serializer_details_class
+        return super().retrieve(request, *args, **kwargs)
